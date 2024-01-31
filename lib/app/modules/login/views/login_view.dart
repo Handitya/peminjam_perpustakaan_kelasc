50f3ea488b4a8a11262ebdd9956e0705b787fbfe
TegarrPM <fadhillahtegarpangestu@gmail.com>
2024-01-31 15:01:12 +0700

    commit

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
+import '../../../routes/app_pages.dart';
 import '../controllers/login_controller.dart';
 
 class LoginView extends GetView<LoginController> {
@@ -13,12 +14,41 @@ class LoginView extends GetView<LoginController> {
         title: const Text('LoginView'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'LoginView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
+      body: Center(
+          child: Form(
+              key: controller.formkey,
+              child: Column(
+                children: [
+                  TextFormField(
+                      controller: controller.usernameController,
+                      decoration: InputDecoration(hintText: "Masukan Username"),
+                      validator: (value) {
+                        if (value!.length < 2) {
+                          return "Username tidak boleh kosong";
+                        }
+                        return null;
+                      }),
+                  TextFormField(
+                      controller: controller.passwordController,
+                      decoration: InputDecoration(hintText: "Masukan Password"),
+                      validator: (value) {
+                        if (value!.length < 2) {
+                          return "Password tidak boleh kosong";
+                        }
+                        return null;
+                      }),
+                  Obx(() => controller.loading.value
+                      ? CircularProgressIndicator()
+                      : ElevatedButton(
+                          onPressed: () {
+                            controller.login();
+                          },
+                          child: Text('Login'))),
+                  ElevatedButton(
+                      onPressed: () => Get.toNamed(Routes.REGISTER),
+                      child: Text("Registrasi"))
+                ],
+              ))),
     );
   }
 }
